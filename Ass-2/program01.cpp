ebe794d0d68d1b607871e3f502942a46a8fa1dab
Sanjay Developer <sureshsanjay805@gmail.com>
2020-08-04 08:19:16 +0530

    [ADDED] Ass-2 => Program 01

---
+++ Ass-2/program01.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+class Student {
+	private:
+		int rollNo;
+		std::string name;
+		double percent;
+		
+	public:
+		void setDetails(int, std::string, double);
+		void printDetails();
+};
+
+void Student::setDetails(int i, std::string n, double d) {
+	rollNo = i;
+	name = n;
+	percent = d;
+}
+
+void Student::printDetails() {
+	std::cout << "Roll no => "<< rollNo << std::endl;
+	std::cout << "Name => " << name << std::endl;
+	std::cout << "Percentage => " << percent << std::endl;
+}
+
+int main() {
+	Student student;
+	int rollNo;
+	std::string name;
+	double percent;
+	std::cout << "Enter student rollno, name and percent" << std::endl;
+	std::cin >> rollNo >> name >> percent;
+	student.setDetails(rollNo, name, percent);
+	student.printDetails();
+	return 0;
+}
