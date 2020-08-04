fd278ae8518da1ab60e061d46998d3e72a764b3f
Sanjay Developer <sureshsanjay805@gmail.com>
2020-08-04 09:12:12 +0530

    [ADDED] Ass-3 => Program 02

---
+++ Ass-3/program02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+class Human {
+   private:
+    std::string name;
+    int age;
+
+   public:
+    void setDetails(std::string s, int i) {
+        name = s;
+        age = i;
+    }
+    std::string getName() { return name; }
+    friend int getAge(Human);
+};
+int getAge(Human h) { return h.age; }
+int main() {
+    Human h1;
+    int age;
+    std::cout << "Enter your name and age" << std::endl;
+    std::string name;
+    std::cin >> name >> age;
+    h1.setDetails(name, age);
+    std::cout << "Name: " << h1.getName() << std::endl;
+    std::cout << "Age: " << getAge(h1) << std::endl;
+    return 0;
+}
